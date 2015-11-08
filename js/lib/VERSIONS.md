c3f228c26a50bc224428ccf7c22f61cdfe65f17c
northlandiguana <northlandiguana@gmail.com>
2015-11-07 20:52:51 -0600

    Leaflet Map view with Choropleth isomorph model implemented; still a bug with initial CSS

---
+++ js/lib/VERSIONS.md
@@ -0,0 +1,23 @@
+# Included library versions
+
+- jQuery 1.11.3
+
+- Underscorejs 1.8.3
+
+- Backbonejs 1.2.3
+
+- Backbone-Deep-Model 0.10.4
+
+- modernizr 2.8.3
+
+- Leaflet 1.0 beta 2
+
+- Mapbox-GL 0.11.1
+
+- D3 3.5.6
+
+- Simple Statistics 1.0.0
+
+# Example files:
+
+- *us-states.geojson* -- shamelessly stolen from [Leaflet Interactive Choropleth Map tutorial](http://leafletjs.com/examples/choropleth.html)
--- js/modules/m-section.js
+++ js/modules/m-section.js
@@ -1,9 +1,122 @@
 //Map panel
 
+(function(){
+
+/************** M-classification ****************/
+
+var Quantile = Backbone.Model.extend({
+	defaults: {
+		type: 'quantile'
+	},
+	scale: function(values, classes){
+		//create scale generator
+		var scale = d3.scale.quantile()
+			.range(classes);
+		//assign array of values as scale domain
+		scale.domain(values);
+		//done
+		return scale;
+	}
+});
+
+var EqualInterval = Backbone.Model.extend({
+	defaults: {
+		type: 'equal interval'
+	},
+	scale: function(values, classes){
+		//create scale generator
+		var scale = d3.scale.quantile()
+			.range(classes);
+		//assign two-value array as scale domain
+		scale.domain([d3.min(values), d3.max(values)]);
+		//done
+		return scale;
+	}
+});
+
+var NaturalBreaks = Backbone.Model.extend({
+	defaults: {
+		type: 'natural breaks'
+	},
+	scale: function(values, classes){
+		//create scale generator
+		var scale = d3.scale.threshold()
+			.range(classes);
+		//cluster data using ckmeans clustering algorithm to create natural breaks
+		var clusters = ss.ckmeans(values, classes.length);
+		//set domain array to cluster minimums
+		var domainArray = clusters.map(function(d){
+			return d3.min(d);
+		});
+		//remove first value from domain array to create class breakpoints
+		domainArray.shift();
+		//assign array of remaining cluster minimums as domain
+		scale.domain(domainArray);
+		//done
+		return scale;
+	}
+});
+
+var Unclassed = Backbone.Model.extend({
+	defaults: {
+		type: 'unclassed'
+	},
+	scale: function(values, rangeBounds){
+		//create scale generator
+		var scale = d3.scale.linear()
+			.range(rangeBounds);
+		//assign two-value array as scale domain
+		scale.domain([d3.min(values), d3.max(values)]);
+		//done
+		return scale;
+	}
+});
+
+//a single collection holds all classification models
+var classification = new Backbone.Collection([
+	new Quantile(),
+	new EqualInterval(),
+	new NaturalBreaks(),
+	new Unclassed()
+]);
+
 /************** M-isomorph ****************/
 
 //model for choropleth data overlay
-var Choropleth = Backbone.Model.extend();
+var Choropleth = Backbone.Model.extend({
+	defaults: {
+		isomorphType: 'choropleth',
+		classificationType: '',
+		classedAttribute: '',
+		classes: '',
+		data: {}
+	},
+	setClasses: function(){
+		var classedAttribute = this.get('classedAttribute');
+		//get all of the values for the attribute by which the data will be classed
+		var values = _.map(this.get('features'), function(feature){
+			return parseFloat(feature.properties[classedAttribute]);
+		});
+		//get the d3 scale for the chosen classification scheme
+		var classificationModel = classification.where({type: this.get('classificationType')})[0];
+		var scale = classificationModel.scale(values, this.get('classes'));
+		//set a new fillColor property for each feature with the class color value
+		this.get('features').forEach(function(feature){
+			feature.properties.layerOptions = {
+				fillColor: scale(parseFloat(feature.properties[classedAttribute]))
+			};
+		});
+		this.trigger('done');
+	},
+	initialize: function(){
+		this.on('sync', this.setClasses);
+	}
+});
+
+//a single collection holds all visual isomorphs
+var isomorph = new Backbone.Collection([
+	new Choropleth()
+]);
 
 //MODELS NEEDED
 //-layers for each isomorph and each library
@@ -17,13 +130,11 @@ var LeafletMap = Backbone.View.extend({
 	events: {
 		//DOM events--not sure I'll use this
 	},
-	initialize: function(options){
+	initialize: function(){
 		//attach an event for each interaction--remember to have user interaction trigger on map
 		_.each(this.model.get('interactions'), function(interaction){
 			this.on(interaction, this.recordInteraction);
 		}, this);
-
-		this.options = options || {};
 	},
 	render: function(){
 		this.$el.html("<div id='map'>");
@@ -39,16 +150,37 @@ var LeafletMap = Backbone.View.extend({
 			this.model.set('mapOptions.boxZoom', false);
 		};
 
-		//add initial map layers
-		var layers = [];
-		var baseTiles = L.tileLayer(this.model.get('baseTiles.url'), this.model.get('baseTiles.layerOptions'));
-		layers.push(baseTiles);
-		var dataLayerModel = eval("new " + this.model.get('dataLayer.isomorph.type') + "()"); //get model based on isomorph type--this seems like the best approach?`
-		layers.push(dataLayerModel.get('layer'));
-		// this.model.set('mapOptions.layers', layers);
-
 		//instantiate map
 		this.map = L.map('map', this.model.get('mapOptions'));
+
+		//add initial map layers
+		var layers = [];
+		var baseTiles = L.tileLayer(this.model.get('baseTiles.url'), this.model.get('baseTiles.layerOptions')).addTo(this.map);
+		layers.push(baseTiles);
+
+		var dataLayerOptions = this.model.get('dataLayer.layerOptions');
+		//get model based on isomorph type
+		var dataLayerModel = isomorph.where({isomorphType: this.model.get('dataLayer.isomorph.type')})[0];
+		//pass in necessary values
+		dataLayerModel.set({
+			classificationType: this.model.get('dataLayer.isomorph.classification'),
+			classedAttribute: this.model.get('dataLayer.isomorph.classedAttribute'),
+			classes: this.model.get('dataLayer.isomorph.classes')
+		});
+		dataLayerModel.on('done', function(){
+			function style(feature){
+				console.log(_.extend(feature.properties.layerOptions, dataLayerOptions));
+				//combine layer options objects from config file and feature properties
+				return _.extend(feature.properties.layerOptions, dataLayerOptions);
+			};
+			//add Leaflet overlay
+			var dataLayer = L.geoJson(dataLayerModel.get('features'), {style: style}).addTo(this.map);
+			layers.push(dataLayer);
+		}, this);
+		//go get the data!
+		dataLayerModel.fetch({url: this.model.get('dataLayer.url')});
+
+		// this.model.set('mapOptions.layers', layers); //can't do this because AJAX in isomorph model
 
 		//layers control
 		var baseLayers = {}, overlays = {};
@@ -78,6 +210,8 @@ var LeafletMap = Backbone.View.extend({
 	}
 });
 
+/************** set map view ****************/
+
 function setMapView(options){
 	var mapView = eval("new " + options.get('library') + "Map({model: options})");
 	mapView.render().setMap();
@@ -93,3 +227,5 @@ var MapOptions = Backbone.DeepModel.extend({
 var options = new MapOptions();
 options.on('sync', setMapView);
 options.fetch();
+
+})();
